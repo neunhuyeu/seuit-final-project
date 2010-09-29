d1a0d6607f9f6b16d026678da16a48d402ce365a
thanhman.gm <thanhman.gm@7db91a31-11c4-2d82-70bf-a487b79eb318>
2010-09-29 18:56:07 +0000

    Add new document of WCF. Do an exercise about basics of WCF.

---
+++ trunk/research/WCF/DemoRentalCarReservation/DemoRentalCarReservation/ReservationHost.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ServiceModel;
+
+namespace DemoRentalCarReservation
+{
+    public class ReservationHost
+    {
+        public static void Main()
+        {
+            ServiceHost s = new ServiceHost(typeof(RentalCarReservation));
+            s.AddServiceEndpoint(typeof(RentalCarReservation), new WSHttpBinding(), "http://localhost/DemoRentalCarReservation_deploy/RentalCarReservation.svc");
+            s.Open();
+            Console.Write("Press ENTER to terminate the service");
+            Console.ReadLine();
+            s.Close();
+        }
+    }
+}
